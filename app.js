b9e82005b798d40cda457c8b5ddd855823c903b8
ddung203 <duongvandung2k3@gmail.com>
2023-01-05 01:07:54 +0700

    finish result function

--- app.js
+++ app.js
@@ -15,44 +15,72 @@ window.addEventListener("load", function () {
     count++;
 });
 console.log(count);
-// display count
-// document.getElementById("visit-count").textContent = count;
+
 
 //! Main Logic processing
-let tx1 = document.getElementById("tx1");
-let trongso1 = document.getElementById("trongso1");
-let tx2 = document.getElementById("tx2");
-let trongso2 = document.getElementById("trongso2");
-let thi = document.getElementById("thi");
-// let kq = document.getElementById("kq");
+function data() {
+    let tx1 = document.getElementById("tx1");
+    let trongso1 = document.getElementById("trongso1");
+    let tx2 = document.getElementById("tx2");
+    let trongso2 = document.getElementById("trongso2");
+    let thi = document.getElementById("thi");
+    let kq = document.getElementById("kq");
+
+
+    let tx1Value = Number(document.getElementById("tx1").value);
+    let trongso1Value = Number(trongso1.value);
+    let tx2Value = Number(document.getElementById("tx2").value);
+    let trongso2Value = Number(trongso2.value);
+    let thiValue = Number(document.getElementById("thi").value);
+    let trongso3Value = 100 - trongso1Value - trongso2Value;
+
+    return [
+        tx1Value, trongso1Value,
+        tx2Value, trongso2Value,
+        thiValue, trongso3Value
+    ];
+}
 
 const yc = document.getElementById("yc");
-const ketqua = document.getElementById("ketqua");
+const resultBtn = document.getElementById("rs-btn");
 
-ketqua.addEventListener("click", () => {
-    let tx1Value = document.getElementById("tx1").value;
-    let trongso1Value = document.getElementById("trongso1").value;
-    let tx2Value = document.getElementById("tx2").value;
-    let trongso2Value = document.getElementById("trongso2").value;
-    let thiValue = document.getElementById("thi").value;
-    let trongso3Value = document.getElementById("trongso3").value;
+resultBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // console.log(data());
     let kqVal = document.getElementById("kq");
 
-    let arr = [];
-    arr.push(
-        tx1Value,
-        trongso1Value,
-        tx2Value,
-        trongso2Value,
-        thiValue,
-        trongso3Value
-    );
+    let arr = data();
     let kq =
         arr[0] * (arr[1] / 100) +
         arr[2] * (arr[3] / 100) +
         arr[4] * (arr[5] / 100);
     kqVal.value = kq;
-    // console.log(kqVal.value);
+    // Xác định thang điểm
+    let rank = ['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'];
+    if (kq < 4) {
+        kqVal.value = `${kq} - ${rank[7]}`;
+    }
+    else if (kq <= 4.9) {
+        kqVal.value = `${kq} - ${rank[6]}`;
+    }
+    else if (kq <= 5.5) {
+        kqVal.value = `${kq} - ${rank[5]}`;
+    }
+    else if (kq <= 6.4) {
+        kqVal.value = `${kq} - ${rank[4]}`;
+    }
+    else if (kq <= 6.9) {
+        kqVal.value = `${kq} - ${rank[3]}`;
+    }
+    else if (kq <= 7.9) {
+        kqVal.value = `${kq} - ${rank[2]}`;
+    }
+    else if (kq <= 8.4) {
+        kqVal.value = `${kq} - ${rank[1]}`;
+    }
+    else if (kq <= 10) {
+        kqVal.value = `${kq} - ${rank[0]}`;
+    }
 });
 
 yc.addEventListener("click", () => {
